ca9aeff000a974727c31d05952cb1a3cf1d9c54b
Alex Urbini <aurbini92@gmail.com>
2020-12-19 14:21:47 -0500

    added custom Form and custom button components

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in'
+
+const SignInAndSignUpPage = () => (  
+  <SignIn className='sign-in-and-sign-up' />  
+);
+
+ 
+export default SignInAndSignUpPage;
